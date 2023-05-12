e4cbe5fe656b623efff587f1e9927f484df72af6
Lion1429 <133425574+Lion1429@users.noreply.github.com>
2023-05-12 15:07:08 -0500

    Update MCM.py

--- MCM.py
+++ MCM.py
@@ -1,9 +1,12 @@
-a = int(input(" "))
-b = int(input(" "))
-mcm = max (a,b):
-while true:
-mcm (c % a == 0) and (c% b == 0)
-return z:
-z +=1
-
-print (mcm)
+def calcular_mcm(a, b):
+    max_val = max(a, b)
+    mcm = max_val
+    while True:
+        if mcm % a == 0 and mcm % b == 0:
+            break
+        mcm += max_val
+    return mcm
+num1 = int(input(" "))
+num2 = int(input(" "))
+resultado = calcular_mcm(num1, num2)
+print(resultado)
